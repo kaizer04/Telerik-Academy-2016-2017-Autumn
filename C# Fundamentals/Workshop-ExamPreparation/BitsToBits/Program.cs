4c16e2c4d6b2cb18c370106e2921fd14263c1260
kaizer04 <kaizer0505@gmail.com>
2016-11-12 14:34:14 +0200

    added Bits To Bits Task from Exam Preparation

--- C# Fundamentals/Workshop-ExamPreparation/BitsToBits/Program.cs
+++ C# Fundamentals/Workshop-ExamPreparation/BitsToBits/Program.cs
@@ -1,16 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BitsToBits
+﻿namespace BitsToBits
 {
-    class Program
+    using System;
+
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
-            
+            int n = int.Parse(Console.ReadLine());
+
+            int maxZeroCount = 0;
+            int currentZeroCount = 0;
+
+            int maxOneCount = 0;
+            int currentOneCount = 0;
+
+            int lastBit = 5;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                for (int j = 29; j >= 0; j--)
+                {
+                    int bit = ((1 << j) & num) >> j;
+                    //Console.Write(bit);
+
+                    if (bit == 1)
+                    {
+                        if (lastBit == 1)
+                        {
+                            currentOneCount++;
+                        }
+                        else
+                        {
+                            maxZeroCount = Math.Max(maxZeroCount, currentZeroCount);
+                            currentZeroCount = 0;
+                            currentOneCount = 1;
+                        }
+                    }
+                    else // bit == 0
+                    {
+                        if (lastBit == 0)
+                        {
+                            currentZeroCount++;
+                        }
+                        else
+                        {
+                            maxZeroCount = Math.Max(maxZeroCount, currentZeroCount);
+                            currentOneCount = 0;
+                            currentZeroCount = 1;
+                        }
+
+                        lastBit = 9;
+                    }
+                }
+
+                //Console.WriteLine();
+            }
         }
     }
 }
