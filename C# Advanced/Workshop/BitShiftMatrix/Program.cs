0432120914e1b56ecb98af489f8380a54fd77063
kaizer04 <kaizer0505@gmail.com>
2016-12-04 19:38:29 +0200

    added solutions for task BitShiftMatrix and Messages of Workshop from 23.11.2016

---
+++ C# Advanced/Workshop/BitShiftMatrix/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace BitShiftMatrix
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        static int rows;
+        static int cols;
+        static int[] moves;
+        static BigInteger[,] field;
+
+        public static void Main()
+        {
+            rows = int.Parse(Console.ReadLine());
+            cols = int.Parse(Console.ReadLine());
+            int movesCount = int.Parse(Console.ReadLine());
+            moves = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            field = new BigInteger[rows, cols];
+            FillMatrix(field);
+            PrintMatrix(field);
+
+            BigInteger sum = 0;
+            int[] pos = { rows - 1, 0 };
+            int coeff = rows > cols ? rows : cols;
+
+            sum += field[pos[0], pos[1]];
+            field[pos[0], pos[1]] = 0;
+
+            foreach (var code in moves)
+            {
+                int[] targetPos = { code / coeff, code % coeff };
+
+                //int stepRow = pos[0] == targetPos[0] ? 0 : (pos[0] < targetPos[0] ? 1 : -1);
+                //int stepCol = pos[1] == targetPos[1] ? 0 : (pos[1] < targetPos[1] ? 1 : -1);
+                int stepRow = Math.Sign(targetPos[0] - pos[0]);
+                int stepCol = Math.Sign(targetPos[1] - pos[1]);
+
+                do
+                {
+                    pos[1] += stepCol;
+                    sum += field[pos[0], pos[1]];
+                    field[pos[0], pos[1]] = 0;
+
+                }
+                while (pos[1] != targetPos[1] && pos[1] >= 0 && pos[1] < cols);
+
+                do
+                {
+                    pos[0] += stepRow;
+                    sum += field[pos[0], pos[1]];
+                    field[pos[0], pos[1]] = 0;
+
+                }
+                while (pos[0] != targetPos[0] && pos[0] >= 0 && pos[0] < cols);
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static void PrintMatrix(BigInteger[,] board)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write("{0,4}", field[row, col]); 
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillMatrix(BigInteger[,] field)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    field[row, col] = 1 << (rows - 1 - row + col);
+                }
+
+            }
+        }
+    }
+}
---
+++ C# Advanced/Workshop/Messages/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Messages
+{
+    using System;
+    using System.Numerics;
+
+    public class Program
+    {
+        static string[] numSystem = { "cad", "xoz", "nop", "cyk", "min", "mar", "kon", "iva", "ogi", "yan" };
+
+        public static void Main()
+        {
+            string firstInputString = Console.ReadLine();
+            string op = Console.ReadLine();
+            string secondInputString = Console.ReadLine();
+
+            BigInteger firstNumber = Decrypt(firstInputString);
+            BigInteger secondNumber = Decrypt(secondInputString);
+
+            BigInteger numResult = (op == "+" ? firstNumber + secondNumber : firstNumber - secondNumber);
+
+            string result = Encrypt(numResult);
+            Console.WriteLine(result);
+        }
+
+        private static string Encrypt(BigInteger number)
+        {
+            string result = string.Empty;
+
+            int digit = 0;
+            while (number > 0)
+            {
+                digit = (int)(number % 10);
+                result = numSystem[digit] + result;
+                number /= 10;
+            }
+
+            return result;
+        }
+
+        private static BigInteger Decrypt(string str)
+        {
+            BigInteger result = 0;
+
+            string digit;
+            for (int j = 0; j < str.Length; j += 3)
+            {
+                digit = str.Substring(j, 3);
+                // result = result * Array.IndexOf(numSystem, digit);
+                for (int i = 0; i < numSystem.Length; i++)
+                {
+                    if (digit == numSystem[i])
+                    {
+                        result = result * 10 + i;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
