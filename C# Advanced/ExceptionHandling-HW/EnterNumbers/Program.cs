d74c2c465693391f1a2c2c7d73af923e5d136368
kaizer04 <kaizer0505@gmail.com>
2016-12-05 15:37:48 +0200

    added ExceptionHandling Homework

---
+++ C# Advanced/ExceptionHandling-HW/EnterNumbers/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace EnterNumbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static int lastNumber = 1;
+        static List<int> numberInRange = new List<int>();
+        const int START = 1;
+        const int END = 100;
+
+        static int ReadNumber(int start, int end)
+        {
+            int number = int.Parse(Console.ReadLine());
+            
+            if ((number < start) || (number > end) || (number <= lastNumber))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            lastNumber = number;
+
+            return number;
+        }
+
+        static void Main()
+        {
+            try
+            {
+                numberInRange.Add(START);
+                for (int i = 1; i <= 10; i++)
+                {
+                    // Console.Write("Enter number {0} in the range [{1}...{2}] : ", i, 1, 100);
+                    int number = ReadNumber(START, END);
+                    numberInRange.Add(number);
+                }
+
+                numberInRange.Add(100);
+                Console.WriteLine(string.Join(" < ", numberInRange));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Exception");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Exception");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Exception");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Exception");
+            }
+        }
+    }
+}
---
+++ C# Advanced/ExceptionHandling-HW/SquareRoot/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SquareRoot
+{
+    using System;
+
+    public class Program
+    {
+        public static double Sqrt(double value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Invalid number");
+            }
+
+            return Math.Sqrt(value);
+        }
+
+        static void Main()
+        {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                Console.WriteLine("{0:F3}", Sqrt(number));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
