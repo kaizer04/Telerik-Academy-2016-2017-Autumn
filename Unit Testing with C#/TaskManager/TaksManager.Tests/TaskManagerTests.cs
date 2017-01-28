a87311a2947821cd1a9d670ba3fd6ae76e7b22b9
kaizer04 <kaizer0505@gmail.com>
2017-01-28 13:43:23 +0200

    Added the 3A Pattern comments

--- Unit Testing with C#/TaskManager/TaksManager.Tests/TaskManagerTests.cs
+++ Unit Testing with C#/TaskManager/TaksManager.Tests/TaskManagerTests.cs
@@ -11,10 +11,13 @@
         [TestMethod]
         public void AddingNewTaskShouldProperlyUpdateTheCount()
         {
+            // Arrange
             var taskManager = new TaskManager();
 
+            // Act
             taskManager.AddTask(new Task()); 
-
+            
+            // Assert
             Assert.AreEqual(1, taskManager.TasksCount, 
                 string.Format("Tasks count is {0} whisch is incorect value.", taskManager.TasksCount));
         }
