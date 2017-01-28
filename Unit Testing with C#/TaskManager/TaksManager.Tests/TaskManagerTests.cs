3aca3f3952e70f0da2512710c5245db309835627
kaizer04 <kaizer0505@gmail.com>
2017-01-28 13:41:26 +0200

    Added Demo Test Unit Project

---
+++ Unit Testing with C#/TaskManager/TaksManager.Tests/TaskManagerTests.cs
@@ -0,0 +1,28 @@
+﻿namespace TaksManager.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using TaskManager;
+
+    [TestClass]
+    public class TaskManagerTests
+    {
+        [TestMethod]
+        public void AddingNewTaskShouldProperlyUpdateTheCount()
+        {
+            var taskManager = new TaskManager();
+
+            taskManager.AddTask(new Task()); 
+
+            Assert.AreEqual(1, taskManager.TasksCount, 
+                string.Format("Tasks count is {0} whisch is incorect value.", taskManager.TasksCount));
+        }
+
+        // not a test method
+        public  void DoSomething()
+        {
+
+        }
+    }
+}
---
+++ Unit Testing with C#/TaskManager/TaskManager/TaskManager.cs
@@ -0,0 +1,29 @@
+﻿namespace TaskManager
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TaskManager
+    {
+        //int count = 0;
+        ICollection<Task> tasks = new List<Task>();
+
+        public int TasksCount
+        {
+            get
+            {
+                //refactoring
+                //return this.count;
+                return tasks.Count;
+            }
+        }
+
+        public void AddTask(Task task)
+        {
+            this.tasks.Add(task);
+            //count++;
+        }
+    }
+}
