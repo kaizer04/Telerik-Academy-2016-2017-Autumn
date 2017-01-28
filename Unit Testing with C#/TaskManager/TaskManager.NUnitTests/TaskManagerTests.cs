b0a3f7ce9974dcd2e7bbd9edc146d556641ff5f0
kaizer04 <kaizer0505@gmail.com>
2017-01-28 20:21:52 +0200

    added NUnit tests

---
+++ Unit Testing with C#/TaskManager/TaskManager.NUnitTests/TaskManagerTests.cs
@@ -0,0 +1,42 @@
+﻿namespace TaskManager.NUnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    
+    using NUnit.Framework;
+    
+    [TestFixture]
+    public class TaskManagerTests
+    {
+        [Test]
+        public void AddingNewNullTaskShouldThrowAnException()
+        {
+            // Arrange
+            var taskManager = new TaskManager();
+            
+            // Assert
+            Assert.Throws(typeof(ArgumentNullException), () => { taskManager.AddTask(null); });
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(13)]
+        public void AddingNewTaskShouldProperlyUpdateTheCount(int count)
+        {
+            // Arrange
+            var taskManager = new TaskManager();
+
+            // Act
+            for (int i = 0; i < count; i++)
+            {
+                taskManager.AddTask(new Task());
+            }
+
+            // Assert
+            Assert.AreEqual(count, taskManager.TasksCount,
+                string.Format("Tasks count is {0} whisch is incorect value.", taskManager.TasksCount));
+        }
+    }
+}
