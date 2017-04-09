52b7a927112c79402dcd66d144eee0cbf0b34a1c
kaizer04 <kaizer0505@gmail.com>
2017-04-09 16:58:10 +0300

    added Defensive Programming and Exceptions Homework

---
+++ High-Quality-Code-Part-2/01. Defensive-Programming-and-Exceptions/Exceptions-Homework/CSharpExam.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class CSharpExam : Exam
+{
+    private const int MinGrade = 0;
+    private const int MaxGrade = 100;
+
+    private int score;
+
+    public CSharpExam(int score)
+    {
+        this.Score = score;
+    }
+
+    public int Score
+    {
+        get
+        {
+            return this.score;
+        }
+
+        set
+        {
+            if (value < MinGrade || value > MaxGrade)
+            {
+                throw new ArgumentException("Score can not be less than minimum grade and bigger than maximum grade!");
+            }
+
+            this.score = value;
+        }
+    }
+
+    public override ExamResult Check()
+    {
+        if (this.Score < MinGrade || this.Score > MaxGrade)
+        {
+            throw new InvalidOperationException("Score can not be less than minimum grade and bigger than maximum grade!");
+        }
+
+        return new ExamResult(this.Score, MinGrade, MaxGrade, "Exam results calculated by score.");
+    }
+}
---
+++ High-Quality-Code-Part-2/01. Defensive-Programming-and-Exceptions/Exceptions-Homework/SimpleMathExam.cs
@@ -0,0 +1,60 @@
+﻿public class SimpleMathExam : Exam
+{
+    private const int MinProblemsSolved = 0;
+    private const int MaxProblemsSolved = 10;
+
+    private int problemsSolved;
+
+    public SimpleMathExam(int problemsSolved)
+    {
+        this.ProblemsSolved = problemsSolved;
+    }
+
+    public int ProblemsSolved
+    {
+        get
+        {
+            if (this.problemsSolved < MinProblemsSolved)
+            {
+                return MinProblemsSolved;
+            }
+            else if (this.problemsSolved > MaxProblemsSolved)
+            {
+                return MaxProblemsSolved;
+            }
+            else
+            {
+                return this.problemsSolved;
+            }
+        }
+
+        set
+        {
+            this.problemsSolved = value;
+        }
+    }
+    
+    public override ExamResult Check()
+    {
+        string comment;
+
+        if (this.ProblemsSolved <= 2)
+        {
+            comment = "Bad result: nothing done.";
+        }
+        else if (this.ProblemsSolved <= 5)
+        {
+            comment = "Average result: almost nothing done.";
+        }
+        else if (this.ProblemsSolved <= 8)
+        {
+            comment = "Good result: almost everything's done correctly.";
+        }
+        else
+        {
+            comment = "Excellent result: everything's done correctly.";
+        }
+
+        return new ExamResult(this.ProblemsSolved, MinProblemsSolved, MaxProblemsSolved, comment);
+    }
+}
