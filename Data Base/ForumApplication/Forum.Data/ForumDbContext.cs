a89cefa608423442bc4cf15c7e1ec54bd694581e
kaizer04 <kaizer0505@gmail.com>
2017-05-21 22:00:56 +0300

    added Forum.Web project

---
+++ Data Base/ForumApplication/Forum.Data/ForumDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+using Forum.Models;
+
+namespace Forum.Data
+{
+    public class ForumDbContext : DbContext
+    {
+        public ForumDbContext()
+            : base("ForumDb")
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<PostAnswer> PostAnswers { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
