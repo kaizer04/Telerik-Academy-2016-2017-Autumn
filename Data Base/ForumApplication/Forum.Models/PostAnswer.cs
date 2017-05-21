1010227744a2c89ba78588f882c6754d5db9fcee
kaizer04 <kaizer0505@gmail.com>
2017-05-21 21:29:33 +0300

    added all models

---
+++ Data Base/ForumApplication/Forum.Models/PostAnswer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Forum.Models
+{
+    public class PostAnswer
+    {
+        public PostAnswer()
+        {
+            this.AnsweredOn = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime AnsweredOn { get; private set; }
+
+        public int PostId { get; set; }
+
+        public virtual Post Post { get; set; }
+    }
+}
---
+++ Data Base/ForumApplication/Forum.Models/Tag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Forum.Models
+{
+    public class Tag
+    {
+        private ICollection<Post> posts;
+
+        public Tag()
+        {
+            this.posts = new HashSet<Post>();
+        }
+
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public ICollection<Post> Posts 
+        {
+            get
+            {
+                return this.posts;
+            }
+
+            set
+            {
+                this.posts = value;
+            }
+        }
+    }
+}
