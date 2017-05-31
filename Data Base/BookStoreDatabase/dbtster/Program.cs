6623eaa19c0fcd5a719b49d616db9742fbcc21cc
kaizer04 <kaizer0505@gmail.com>
2017-05-31 20:24:18 +0300

    added the project

---
+++ Data Base/BookStoreDatabase/dbtster/Program.cs
@@ -0,0 +1,27 @@
+﻿using BookStoreDatabase;
+using System.Data.Entity;
+using dbtster.Migrations;
+
+namespace dbtster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BookStoreDbContext, Configuration>());
+            using (var db = new BookStoreDbContext())
+            {
+                var publisher = new Publisher("Pesho");
+                db.Publishers.Add(publisher);
+
+                var genre = new Genre("fiction");
+                db.Genres.Add(genre);
+
+                var book = new Book("Layla", "Pirsig");
+                db.BBooks.Add(book);
+                db.SaveChanges();
+            }
+        }
+    }
+}
+
