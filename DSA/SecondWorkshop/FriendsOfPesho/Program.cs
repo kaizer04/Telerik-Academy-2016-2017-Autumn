5519fc9f46841a1df825c383613f27a5e870c694
kaizer04 <kaizer0505@gmail.com>
2017-07-12 08:09:11 +0300

    added FriendsOfPesho solution

---
+++ DSA/SecondWorkshop/FriendsOfPesho/Program.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendsOfPesho
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] inputNumbers = Console.ReadLine().Split(' ');
+
+            int pointsNumber = int.Parse(inputNumbers[0]);
+            int streetsNumber = int.Parse(inputNumbers[1]);
+            int hoispitalsNumber = int.Parse(inputNumbers[2]);
+
+            string[] allHospitals = Console.ReadLine().Split(' ');
+
+            var graph = new Dictionary<Node, List<Connection>>();
+            var allNodes = new Dictionary<int, Node>();
+
+            for (int i = 0; i < streetsNumber; i++)
+            {
+                string[] currentStreet = Console.ReadLine().Split(' ');
+
+                int firstNode = int.Parse(currentStreet[0]);
+                int secondNode = int.Parse(currentStreet[1]);
+                int distance = int.Parse(currentStreet[2]);
+
+                if (!allNodes.ContainsKey(firstNode))
+                {
+                    allNodes.Add(firstNode, new Node(firstNode));
+                }
+
+                if (!allNodes.ContainsKey(secondNode))
+                {
+                    allNodes.Add(secondNode, new Node(secondNode));
+                }
+
+                var firstNodeObject = allNodes[firstNode];
+                var secondNodeObject = allNodes[secondNode];
+
+                if (!graph.ContainsKey(firstNodeObject))
+                {
+                    graph.Add(firstNodeObject, new List<Connection>());
+                }
+
+                if (!graph.ContainsKey(secondNodeObject))
+                {
+                    graph.Add(secondNodeObject, new List<Connection>());
+                }
+
+                graph[firstNodeObject].Add(new Connection(secondNodeObject, distance));
+                graph[secondNodeObject].Add(new Connection(firstNodeObject, distance));
+            }
+
+            for (int i = 0; i < allHospitals.Length; i++)
+            {
+                int currentHospital = int.Parse(allHospitals[i]);
+
+                allNodes[currentHospital].IsHospital = true;
+            }
+
+            long result = long.MaxValue;
+            for (int i = 0; i < allHospitals.Length; i++)
+            {
+                int currentHospital = int.Parse(allHospitals[i]);
+
+                DijkstraAlgorithm(graph, allNodes[currentHospital]);
+
+                long tempSum = 0;
+                foreach (var node in allNodes)
+                {
+                    if (!node.Value.IsHospital)
+                    {
+                        tempSum += node.Value.DijkstraDistance;
+                    }
+                }
+
+                if (tempSum < result)
+                {
+                    result = tempSum;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static void DijkstraAlgorithm(Dictionary<Node, List<Connection>> graph, Node source)
+        {
+            PriorityQueue<Node> queue = new PriorityQueue<Node>();
+
+            foreach (var node in graph)
+            {
+                node.Key.DijkstraDistance = long.MaxValue;
+            }
+
+            source.DijkstraDistance = 0;
+            queue.Enqueue(source);
+
+            while (queue.Count != 0)
+            {
+                Node currentNode = queue.Dequeue();
+
+                if (currentNode.DijkstraDistance == long.MaxValue)
+                {
+                    break;
+                }
+
+                foreach (var connection in graph[currentNode])
+                {
+                    var potDistance = currentNode.DijkstraDistance + connection.Distance;
+
+                    if (potDistance < connection.ToNode.DijkstraDistance)
+                    {
+                        connection.ToNode.DijkstraDistance = potDistance;
+                        queue.Enqueue(connection.ToNode);
+                    }
+                }
+            }
+        }
+    }
+
+    public class Node : IComparable
+    {
+        public Node(int id)
+        {
+            this.Id = id;
+            this.IsHospital = false;
+        }
+
+        public int Id { get; set; }
+
+        public bool IsHospital { get; set; }
+
+        public long DijkstraDistance { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return this.DijkstraDistance.CompareTo((obj as Node).DijkstraDistance);
+        }
+    }
+
+    public class Connection
+    {
+        public Connection(Node toNode, long distance)
+        {
+            this.ToNode = toNode;
+            this.Distance = distance;
+        }
+
+        public Node ToNode { get; set; }
+
+        public long Distance { get; set; }
+    }
+
+    public class PriorityQueue<T> where T : IComparable
+    {
+        private T[] heap;
+        private int index;
+
+        public PriorityQueue()
+        {
+            this.heap = new T[16];
+            this.index = 1;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.index - 1;
+            }
+        }
+
+        public void Enqueue(T element)
+        {
+            if (this.index >= this.heap.Length)
+            {
+                this.IncreaseArray();
+            }
+
+            this.heap[this.index] = element;
+
+            int childIndex = this.index;
+            int parentIndex = childIndex / 2;
+            this.index++;
+
+            while (parentIndex >= 1 && this.heap[childIndex].CompareTo(this.heap[parentIndex]) < 0)
+            {
+                T swapValue = this.heap[parentIndex];
+                this.heap[parentIndex] = this.heap[childIndex];
+                this.heap[childIndex] = swapValue;
+
+                childIndex = parentIndex;
+                parentIndex = childIndex / 2;
+            }
+        }
+
+        public T Dequeue()
+        {
+            T result = this.heap[1];
+
+            this.heap[1] = this.heap[this.Count];
+            this.index--;
+
+            int rootIndex = 1;
+
+            while (true)
+            {
+                int leftChildIndex = rootIndex * 2;
+                int rightChildIndex = (rootIndex * 2) + 1;
+
+                if (leftChildIndex > this.index)
+                {
+                    break;
+                }
+
+                int minChild;
+                if (rightChildIndex > this.index)
+                {
+                    minChild = leftChildIndex;
+                }
+                else
+                {
+                    if (this.heap[leftChildIndex].CompareTo(this.heap[rightChildIndex]) < 0)
+                    {
+                        minChild = leftChildIndex;
+                    }
+                    else
+                    {
+                        minChild = rightChildIndex;
+                    }
+                }
+
+                if (this.heap[minChild].CompareTo(this.heap[rootIndex]) < 0)
+                {
+                    T swapValue = this.heap[rootIndex];
+                    this.heap[rootIndex] = this.heap[minChild];
+                    this.heap[minChild] = swapValue;
+
+                    rootIndex = minChild;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            return this.heap[1];
+        }
+
+        private void IncreaseArray()
+        {
+            var copiedHeap = new T[this.heap.Length * 2];
+
+            for (int i = 0; i < this.heap.Length; i++)
+            {
+                copiedHeap[i] = this.heap[i];
+            }
+
+            this.heap = copiedHeap;
+        }
+    }
+}
